9d0f919cbc4126792e691a50ebce701a11e9964a
Eugen <eugen252009@gmail.com>
2024-05-13 23:59:47 +0200

    reading from stdin

--- c/lua.lua
+++ c/lua.lua
@@ -1,4 +1,26 @@
 local function hello(i)
 	print(i)
 end
+
+local function hellow(i)
+	print(i)
+end
+
+local function hellowo(i)
+	print(i)
+end
+local function hellowor(i)
+	print(i)
+end
+local function helloworl(i)
+	print(i)
+end
+local function helloworld(i)
+	print(i)
+end
 hello("Hallo Welt!")
+hellow("Hallo Welt!")
+hellowo("Hallo Welt!")
+hellowor("Hallo Welt!")
+helloworl("Hallo Welt!")
+helloworld("Hallo Welt!")
--- c/src/compiler.c
+++ c/src/compiler.c
@@ -1,38 +1,13 @@
-#include <errno.h>
-#include <fcntl.h>
 #include <stdio.h>
-#include <sys/stat.h>
-#include <sys/types.h>
 #include <unistd.h>
-
-#define BUFSIZE 256
+#define BUF_SIZE 2048
 
 int main(int argc, char *argv[]) {
-  int fd = 0;
-  char buf[BUFSIZE];
-  ssize_t size;
-  int errsv = 0;
-  if (argc >= 2) {
-    fd = open(argv[1], O_RDONLY);
-    if (fd == -1) {
-      errsv = errno;
-      printf("%s", "Error Occured while opening file");
-      printf("open() failed with errno Code: %d\n", errsv);
-      return 1;
-    }
-  }
+  size_t n = 0;
+  char buf[BUF_SIZE];
   do {
-    size = read(fd, buf, BUFSIZE);
-    if (size == -1) {
-      int errsv = errno;
-      printf("%s", "Error Occure\n");
-      printf("read() failed with errno Code: %d\n", errsv);
-      return 1;
-    }
-    for (size_t i = 0; i < size; i++) {
-      printf("%c", buf[i]);
-    }
-  } while (size > 0);
-
+    n = read(0, &buf, BUF_SIZE);
+    printf("%s", buf);
+  } while (n > 0);
   return 0;
 }
